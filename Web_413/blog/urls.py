0b3fac52bdae6dde1e137308607613904e305b83
Masha00000 <143980697+Masha00000@users.noreply.github.com>
2024-12-19 00:56:33 +0700

    Update urls.py

--- Web_413/blog/urls.py
+++ Web_413/blog/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
+     path('article/<int:id>/', views.detail, name='article_detail'), #новый маршрут для просмотра статьи
 ]
